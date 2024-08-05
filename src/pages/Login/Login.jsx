588ee43a64f1c46cece0c70db68dd6be3cd7d496
Luiz Felipe Soriano <132586339+lfelipesbs@users.noreply.github.com>
2024-08-05 17:31:44 -0300

    Add files via upload

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/style-prop-object */
 import React from "react";
 import logo from '../../assets/img/logo.png';
 import Button from "../../components/Button/Button";
@@ -10,14 +11,16 @@ function Login() {
     const [page, setPage] = useState('init');
 
     const handleClick = (e) => {
-        switch(e.target.className){
-            case'init-btn-criar-conta':
+        // eslint-disable-next-line default-case
+        switch (e.target.id) {
+            case 'init-btn-criar-conta':
                 setPage('criarConta');
                 break;
             case 'init-btn-login':
                 setPage('login');
                 break;
-            default:
+            case 'voltar':
+                setPage('init');
                 break;
         }
     }
@@ -29,10 +32,10 @@ function Login() {
                 <Image className='logo-init' src={logo} alt="Logo do Tuipixo" />
                 <div className="btns-init">
                     <h2>Inscreva-se hoje</h2>
-                    <Button className='init-btn-criar-conta' onClick={handleClick} style="primary">
+                    <Button id='init-btn-criar-conta' onClick={handleClick} style="first">
                         Criar conta
                     </Button>
-                    <Button className='init-btn-login' onClick={handleClick} style="secondary">
+                    <Button id='init-btn-login' onClick={handleClick} style="second">
                         Entrar
                     </Button>
                 </div>
@@ -41,49 +44,55 @@ function Login() {
     );
 
     const criarConta = () => (
-        <div className="container-criar-conta">
-            <h1>Criar Conta</h1>
-            <div className="container-criar-conta-form">
-                <h2>Insira suas informacoes</h2>
-                <form className='criar-conta-form' action=''>
-                    <label htmlFor="">Nome</label>
-                    <input type="text" name="" id="" />
+        <>
+            <Button id="voltar" onClick={handleClick} style={"third"}>Voltar</Button>
+            <div className="container-criar-conta">
+                <h1>Criar Conta</h1>
+                <div className="container-criar-conta-form">
+                    <h2>Insira suas informacoes: </h2>
+                    <form className='criar-conta-form' action=''>
+                        <label htmlFor="">Nome:</label>
+                        <input type="text" name="" id="" />
 
-                    <label htmlFor="">Email</label>
-                    <input type="email" name="" id="" />
+                        <label htmlFor="">Email:</label>
+                        <input type="email" name="" id="" />
 
-                    <label htmlFor="">Nome de Usuario</label>
-                    <input type="text" name="" id="" />
+                        <label htmlFor="">Nome de Usuario:</label>
+                        <input type="text" name="" id="" />
 
-                    <label htmlFor="">Senha</label>
-                    <input type="password" name="" id="" />
+                        <label htmlFor="">Senha:</label>
+                        <input type="password" name="" id="" />
 
-                    <Button type='submit' style="primary">
-                        Criar conta
-                    </Button>
-                </form>
+                        <Button type='submit' style="first">
+                            Criar conta
+                        </Button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 
     const login = () => (
-        <div className="container-login">
-            <h1>Entrar na conta</h1>
-            <div className="container-login-form">
-                <h2>Insira suas informacoes</h2>
-                <form action="">
-                    <label htmlFor="">Nome de usuario ou email</label>
-                    <input type="text" name="" id="" />
+        <>
+            <Button id="voltar" onClick={handleClick} style={"third"}>Voltar</Button>
+            <div className="container-criar-conta">
+                <h1>Entrar na conta</h1>
+                <div className="container-criar-conta-form">
+                    <h2>Insira suas informacoes:</h2>
+                    <form className='criar-conta-form' action="">
+                        <label htmlFor="">Nome de usuario ou email:</label>
+                        <input type="text" name="" id="" />
 
-                    <label htmlFor="">Senha</label>
-                    <input type="password" name="" id="" />
+                        <label htmlFor="">Senha:</label>
+                        <input type="password" name="" id="" />
 
-                    <Button type='submit' style="primary">
-                        Entrar
-                    </Button>
-                </form>
+                        <Button type='submit' style="first">
+                            Entrar
+                        </Button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 
     return (
